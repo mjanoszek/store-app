72a2387c47c07cb0f93ec944e810fa0ef5c99b27
mjanoszek <mjanoszek@protonmail.com>
2023-08-25 10:35:31 +0200

    chore: organize image imports for home page

---
+++ public/home/import.tsx
@@ -0,0 +1,21 @@
+import appleCard from './appleCard.jpeg';
+import appleWatch from './appleWatch8.jpeg';
+import backToSchool from './backToSchool.jpeg';
+import iPadPro from './iPadPro.jpeg';
+import iPhone14 from './iPhone14.jpeg';
+import iPhone14Pro from './iPhone14Pro.jpeg';
+import iPhoneTradeIn from './iPhoneTradeIn.jpeg';
+import macbookAir from './macbookAir.jpeg';
+import tvPlus from './tvPlus.jpeg';
+
+export const homePageImages = [
+  appleCard,
+  appleWatch,
+  backToSchool,
+  iPadPro,
+  iPhone14,
+  iPhone14Pro,
+  iPhoneTradeIn,
+  macbookAir,
+  tvPlus,
+];
