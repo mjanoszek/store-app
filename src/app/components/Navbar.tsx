fca07e86a5e816de773be8e0599e7baef2590695
mjanoszek <mjanoszek@protonmail.com>
2023-09-01 12:21:21 +0200

    style: apply animations using framer-motion

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,17 @@
+'use client';
 import Link from 'next/link';
 import React from 'react';
 import { FaApple } from 'react-icons/fa6';
 import Searchbar from './Searchbar';
 import CheckoutBag from './CheckoutBag';
 import NavigationMenu from './Navigation/NavigationMenu';
+import { motion } from 'framer-motion';
+
+const menuItemVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
 
 const Navbar = () => {
   return (
@@ -12,28 +20,28 @@ const Navbar = () => {
         <div className='flex w-full flex-row text-sm text-gray-600 '>
           <ul className='flex w-full items-center justify-between font-light md:hidden'>
             <div className='flex items-center'>
-              <li className='mr-6'>
+              <motion.li {...menuItemVariants} className='mr-6'>
                 <Link href='/'>
                   <FaApple />
                 </Link>
-              </li>
+              </motion.li>
             </div>
             <div className='flex '>
               <div className='flex items-center'>
-                <li className='mr-6'>
+                <motion.li {...menuItemVariants} className='mr-6'>
                   <Link href='/search'>
                     <Searchbar />
                   </Link>
-                </li>
+                </motion.li>
               </div>
               <div className='flex items-center'>
-                <li className='mr-6'>
+                <motion.li {...menuItemVariants}>
                   <Link href='/bag'>
                     <CheckoutBag />
                   </Link>
-                </li>
+                </motion.li>
               </div>
-              <div className='flex items-center'>
+              <div className='flex items-center ml-5'>
                 <li>
                   <NavigationMenu />
                 </li>
@@ -42,76 +50,76 @@ const Navbar = () => {
           </ul>
 
           <ul className='hidden items-center gap-7 font-light md:flex'>
-            <li>
+            <motion.li {...menuItemVariants}>
               <Link href='/'>
                 <FaApple />
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/store'>
                 <p>Store</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/mac'>
                 <p>Mac</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/ipad'>
                 <p>iPad</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/iphone'>
                 <p>iPhone</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/watch'>
                 <p>Watch</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/apple-vision-pro'>
                 <p>Vision</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/airpods'>
                 <p>AirPods</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/tv-home'>
                 <p>TV & Home</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/services'>
                 <p>Entertainment</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/accessories'>
                 <p>Accessories</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/support'>
                 <p>Support</p>
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/search'>
                 <Searchbar />
               </Link>
-            </li>
-            <li>
+            </motion.li>
+            <motion.li {...menuItemVariants}>
               <Link href='/bag'>
                 <CheckoutBag />
               </Link>
-            </li>
+            </motion.li>
           </ul>
         </div>
       </div>
