1193281d1404ab2720e3fce0c2bb4b5eeaf343d5
mjanoszek <mjanoszek@protonmail.com>
2023-09-01 11:19:17 +0200

    feat: creates animated list of mobile navigation using framer-motion

--- src/app/components/Navigation/NavigationAnimatedList.tsx
+++ src/app/components/Navigation/NavigationAnimatedList.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import * as React from 'react';
+import { motion } from 'framer-motion';
+import NavigationItem from './NavigationItem';
 
-const NavigationAnimatedList = () => {
-  return <div>NavigationAnimatedList</div>;
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
 };
+
+const NavigationAnimatedList = () => (
+  <motion.ul
+    variants={variants}
+    className='absolute left-0 top-12 z-40 w-screen '
+  >
+    <NavigationItem />
+  </motion.ul>
+);
 
 export default NavigationAnimatedList;
