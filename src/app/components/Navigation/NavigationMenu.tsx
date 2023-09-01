3271dc421b8a12e6354a66e40bcf68211371a139
mjanoszek <mjanoszek@protonmail.com>
2023-09-01 11:16:06 +0200

    feat: create an animated mobile navigation menu with open and close functionality using framer motion library

--- src/app/components/Navigation/NavigationMenu.tsx
+++ src/app/components/Navigation/NavigationMenu.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+'use client';
+import * as React from 'react';
+import { useRef } from 'react';
+import { motion, useCycle } from 'framer-motion';
+import NavigationToggle from './NavigationToggle';
+import NavigationAnimatedList from './NavigationAnimatedList';
+
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 100% 0)`,
+    top: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: 'circle(30px at 50% 0)',
+    top: 0,
+    transition: {
+      delay: 0.1,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
 
 const NavigationMenu = () => {
-  return <div>NavigationMenu</div>;
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+
+  return (
+    <motion.nav
+      initial={false}
+      animate={isOpen ? 'open' : 'closed'}
+      ref={containerRef}
+    >
+      <motion.div
+        variants={sidebar}
+        className='absolute bottom-0 right-0  w-screen bg-white'
+      />
+      <NavigationAnimatedList />
+      <NavigationToggle toggle={() => toggleOpen()} />
+    </motion.nav>
+  );
 };
 
 export default NavigationMenu;
