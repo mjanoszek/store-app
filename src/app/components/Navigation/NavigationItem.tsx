bfb8d53db3938e61b8214a3a16bc04a9fb12ac54
mjanoszek <mjanoszek@protonmail.com>
2023-09-01 14:09:59 +0200

    feat: improve transition animations on navigation items

--- src/app/components/Navigation/NavigationItem.tsx
+++ src/app/components/Navigation/NavigationItem.tsx
@@ -4,18 +4,23 @@ import Link from 'next/link';
 
 const variants = {
   open: {
-    y: 0,
-    opacity: 1,
     transition: {
-      y: { stiffness: 1000, velocity: -100 },
+      staggerChildren: 0.03,
     },
   },
   closed: {
-    y: 50,
-    opacity: 0,
     transition: {
-      y: { stiffness: 1000 },
+      staggerDirection: -1,
     },
+  },
+};
+
+const linkVariants = {
+  open: {
+    opacity: 1,
+  },
+  closed: {
+    opacity: 0,
   },
 };
 
@@ -37,15 +42,19 @@ const NavigationItem = () => {
   return (
     <motion.li
       variants={variants}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.95 }}
       className='z-10 mb-20 flex cursor-pointer list-none items-center p-10 '
     >
       <div className='flex flex-col gap-5'>
         {links.map((link, index) => (
-          <Link key={index} href={link.href}>
-            <p className='text-black font-bold text-2xl'>{link.label}</p>
-          </Link>
+          <motion.div
+            key={index}
+            variants={linkVariants}
+            className='text-2xl font-bold text-black'
+          >
+            <Link href={link.href}>
+              <p>{link.label}</p>
+            </Link>
+          </motion.div>
         ))}
       </div>
     </motion.li>
