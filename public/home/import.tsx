e221b6a4a4963ef11600075058875fd1773cc4e5
mjanoszek <mjanoszek@protonmail.com>
2023-09-04 09:33:53 +0200

    feat: import new images

--- public/home/import.tsx
+++ public/home/import.tsx
@@ -6,12 +6,14 @@ import iPhone14 from './iPhone14.jpeg';
 import iPhone14Pro from './iPhone14Pro.jpeg';
 import iPhoneTradeIn from './iPhoneTradeIn.jpeg';
 import macbookAir from './macbookAir.jpeg';
+import appleEvent from './AppleEvent.jpeg';
 import tvPlus from './tvPlus.jpeg';
 
 export const homePageImages = {
   appleCard,
   appleWatch,
   backToSchool,
+  appleEvent,
   iPadPro,
   iPhone14,
   iPhone14Pro,
