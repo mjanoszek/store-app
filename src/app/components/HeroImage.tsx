9aa0eb226f1eee76162104488076044365ffcdde
mjanoszek <mjanoszek@protonmail.com>
2023-09-04 09:26:10 +0200

    feat: update HeroImage component using Next JS Image component

--- src/app/components/HeroImage.tsx
+++ src/app/components/HeroImage.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Image, { StaticImageData } from 'next/image';
 
-const HeroImage = () => {
-  return <div>HeroImage</div>;
+interface HeroImageProps {
+  imageSrc: StaticImageData;
+  imageAlt: string;
+}
+
+const HeroImage = ({ imageSrc, imageAlt }: HeroImageProps) => {
+  return (
+    <Image
+      src={imageSrc}
+      alt={imageAlt}
+      sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+      height={548}
+      width={734}
+      loading='lazy'
+    />
+  );
 };
 
 export default HeroImage;
