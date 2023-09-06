25b826df976026c28c598ef27e0ddbed3d9a7e3f
mjanoszek <mjanoszek@protonmail.com>
2023-09-06 16:54:32 +0200

    feat: customize wrapper component, add interface and set the default isLarge value as true

--- src/app/components/HeroSectionWrapper.tsx
+++ src/app/components/HeroSectionWrapper.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import HeroHeading from './HeroHeading';
+import HeroDescription from './HeroDescription';
+import CTAButton from './CTAButton';
 
-const HeroSectionWrapper = () => {
-  return <div>HeroSectionWrapper</div>;
+//add headingColor
+interface HeroSectionWrapperProps {
+  SectionBackgroundColor: string;
+  heading: string;
+  description: string;
+  contentColor?: 'white' | 'black';
+  isLarge?: boolean;
+  CTAButtonLabel: string;
+  SecondCTAButtonLabel?: string;
+  imageSrc: string;
+}
+
+const HeroSectionWrapper = ({
+  heading,
+  description,
+  CTAButtonLabel,
+  isLarge = true,
+  SecondCTAButtonLabel,
+  contentColor,
+  imageSrc,
+  SectionBackgroundColor,
+}: HeroSectionWrapperProps) => {
+  const containerStyle = {
+    backgroundImage: `url(${imageSrc})`,
+    backgroundColor: SectionBackgroundColor,
+  };
+
+  return (
+    <div
+      className={`w-full bg-cover bg-center ${
+        isLarge
+          ? 'h-[500px] md:h-[490px] lg:h-[580px]'
+          : 'h-[512px] md:h-[662px] lg:h-[692px]'
+      }`}
+      style={containerStyle}
+    >
+      <div
+        className={`flex flex-col items-center justify-center pt-12 text-center text-black`}
+      >
+        <div className={`text-${contentColor} flex max-w-xl flex-col gap-1 `}>
+          <HeroHeading heading={heading} isLarge={isLarge} />
+          <HeroDescription description={description} isLarge={isLarge} />
+          <div className='flex justify-center gap-5'>
+            <CTAButton label={CTAButtonLabel} url={'back-to-school'} />
+            {SecondCTAButtonLabel && (
+              <CTAButton label={SecondCTAButtonLabel} url={'back-to-school'} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSectionWrapper;
