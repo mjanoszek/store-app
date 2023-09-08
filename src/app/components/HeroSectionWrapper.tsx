5ec19b23552a1a7fe69f2ee300d9aecdf056c0c1
mjanoszek <mjanoszek@protonmail.com>
2023-09-08 10:38:56 +0200

    refactor: Update component structure and styling

--- src/app/components/HeroSectionWrapper.tsx
+++ src/app/components/HeroSectionWrapper.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import HeroHeading from './HeroHeading';
 import HeroDescription from './HeroDescription';
 import CTAButton from './CTAButton';
+import Image from 'next/image';
 
 interface HeroSectionWrapperProps {
   SectionBackgroundColor: string;
+  headingType?: 'text' | 'image';
   heading: string;
+  headingImage?: string;
   description: string | JSX.Element;
   isLarge?: boolean;
   CTAButtonLabel: string;
@@ -16,6 +19,8 @@ interface HeroSectionWrapperProps {
 
 const HeroSectionWrapper = ({
   heading,
+  headingType = 'text',
+  headingImage,
   description,
   CTAButtonLabel,
   isLarge = true,
@@ -39,7 +44,8 @@ const HeroSectionWrapper = ({
       style={containerStyle}
     >
       <div
-        className={`flex h-full flex-col items-center py-8 text-center ${
+        className={`flex h-full
+        flex-col items-center py-10 text-center ${
           isLarge ? 'items-center' : 'items-end '
         }`}
       >
@@ -48,9 +54,23 @@ const HeroSectionWrapper = ({
             isLarge
               ? 'max-w-[280px] text-black md:max-w-md lg:max-w-lg '
               : 'max-w-[280px] text-white md:max-w-xs lg:max-w-md '
-          } flex h-full flex-col gap-1 justify-${textPosition}`}
+          } flex h-full flex-col gap-1 text-center justify-${textPosition}`}
         >
-          <HeroHeading heading={heading} isLarge={isLarge} />
+          {headingType === 'text' && (
+            <HeroHeading heading={heading} isLarge={isLarge} />
+          )}
+          {headingType === 'image' && (
+            <div className='flex items-center justify-center'>
+              <Image
+                src={headingImage}
+                alt='image'
+                sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                height={42}
+                width={109}
+                loading='lazy'
+              />
+            </div>
+          )}
           <HeroDescription description={description} isLarge={isLarge} />
           <div className='flex justify-center gap-5'>
             <CTAButton
