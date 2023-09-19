9a35c29e9cef68aae1f50fd771b7e9f2621189a6
mjanoszek <mjanoszek@protonmail.com>
2023-09-19 11:44:56 +0200

    feat: enhanced component to accept footerText and footerLegal as props and render them

--- src/app/components/FooterWrapper.tsx
+++ src/app/components/FooterWrapper.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import ParagraphDisplay from './ParagraphDisplay';
+import SiteDirectory from './SiteDirectory';
 
-const FooterWrapper = () => {
-  return <div>FooterWrapper</div>;
+interface FooterWrapperProps {
+  footerText: (string | ReactNode)[];
+  footerLegal: (string | ReactNode)[];
+}
+
+const FooterWrapper = ({ footerText, footerLegal }: FooterWrapperProps) => {
+  return (
+    <div className='flex flex-col justify-center gap-5  self-center px-6 lg:mx-auto lg:max-w-[980px] lg:px-0 '>
+      <ParagraphDisplay text={footerText} />
+      <SiteDirectory />
+      <ParagraphDisplay text={footerLegal} />
+    </div>
+  );
 };
 
 export default FooterWrapper;
