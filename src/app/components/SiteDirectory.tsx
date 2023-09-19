4e393fccaaa476798dd0d0ac068c01d630f73a93
mjanoszek <mjanoszek@protonmail.com>
2023-09-19 09:35:40 +0200

    feat: added mobile version with Framer Motion animations

--- src/app/components/SiteDirectory.tsx
+++ src/app/components/SiteDirectory.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { motion, AnimatePresence } from 'framer-motion';
+import { FaXmark, FaPlus } from 'react-icons/fa6';
 
 const categories = [
   {
@@ -121,6 +124,8 @@ const categories = [
 ];
 
 const SiteDirectory = () => {
+  const isMediumScreen = useMediaQuery({ minWidth: 735 });
+
   const column1 = categories.slice(0, 2);
   const column2 = categories.slice(2, 4);
   const column3 = categories.slice(4, 5);
@@ -129,10 +134,66 @@ const SiteDirectory = () => {
 
   const columns = [column1, column2, column3, column4, column5];
 
-  return (
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+
+  const handleCategoryClick = (categoryName: string) => {
+    setActiveCategory(categoryName === activeCategory ? null : categoryName);
+  };
+
+  return !isMediumScreen ? (
+    <div className='flex flex-col flex-nowrap'>
+      {categories.map((category, categoryIndex) => (
+        <div
+          key={categoryIndex}
+          className='flex h-full w-full flex-col items-start justify-center pb-2'
+        >
+          <div className='w-full border-t border-gray-300' />
+
+          <div key={category.categoryName} className='w-full'>
+            <div
+              className='flex w-full items-center justify-between'
+              onClick={() => handleCategoryClick(category.categoryName)}
+            >
+              <h3
+                className={`text-thin flex-1 cursor-pointer py-4 text-xs ${
+                  activeCategory === category.categoryName
+                    ? 'text-blue-500'
+                    : ''
+                }`}
+              >
+                {category.categoryName}
+              </h3>
+              {activeCategory ? <FaXmark /> : <FaPlus />}
+            </div>
+            <AnimatePresence>
+              {activeCategory === category.categoryName && (
+                <motion.ul
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  className='flex flex-col gap-3 overflow-hidden pb-2 text-xs font-thin'
+                >
+                  {category.directories.map((directory, directoryIndex) => (
+                    <motion.li
+                      key={directoryIndex}
+                      initial={{ opacity: 0, y: -10 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: 0 }}
+                    >
+                      <Link href={directory.link}>{directory.name}</Link>
+                    </motion.li>
+                  ))}
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          </div>
+        </div>
+      ))}
+    </div>
+  ) : (
     <div className='flex flex-nowrap'>
       {columns.map((column, columnIndex) => (
-        <div key={columnIndex} className='w-1/5 pr-4 flex flex-col gap-5'>
+        <div key={columnIndex} className='flex w-1/5 flex-col gap-5 pr-4'>
           {column.map((category) => (
             <div key={category.categoryName}>
               <h3 className='text-thin my-2 text-xs'>
