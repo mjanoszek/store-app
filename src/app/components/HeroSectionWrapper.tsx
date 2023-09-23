f66d3332f18ef4b4db9ae12803db25934bd9fbe2
mjanoszek <mjanoszek@protonmail.com>
2023-09-23 11:16:03 +0200

    feat: add CTAButtonLink and textColor props in order to customize appearance

--- src/app/components/HeroSectionWrapper.tsx
+++ src/app/components/HeroSectionWrapper.tsx
@@ -12,9 +12,11 @@ interface HeroSectionWrapperProps {
   description: string | JSX.Element;
   isLarge?: boolean;
   CTAButtonLabel: string;
+  CTAButtonLink: string;
   SecondCTAButtonLabel?: string;
   imageSrc: string;
   textPosition?: 'end' | 'start' | 'center';
+  textColor?: 'black' | 'white';
 }
 
 const HeroSectionWrapper = ({
@@ -23,11 +25,13 @@ const HeroSectionWrapper = ({
   headingImage,
   description,
   CTAButtonLabel,
+  CTAButtonLink,
   isLarge = true,
   textPosition = 'start',
   SecondCTAButtonLabel,
   imageSrc,
   SectionBackgroundColor,
+  textColor,
 }: HeroSectionWrapperProps) => {
   const containerStyle = {
     backgroundImage: `url(${imageSrc})`,
@@ -52,12 +56,16 @@ const HeroSectionWrapper = ({
         <div
           className={`${
             isLarge
-              ? 'max-w-[280px] text-black md:max-w-md lg:max-w-lg '
-              : 'max-w-[280px] text-white md:max-w-xs lg:max-w-md '
-          } flex h-full flex-col gap-1 text-center justify-${textPosition}`}
+              ? 'max-w-[280px] md:max-w-md lg:max-w-lg '
+              : 'max-w-[280px] md:max-w-xs lg:max-w-md '
+          } flex h-full flex-col gap-1 text-center text-white justify-${textPosition}`}
         >
           {headingType === 'text' && (
-            <HeroHeading heading={heading} isLarge={isLarge} />
+            <HeroHeading
+              heading={heading}
+              isLarge={isLarge}
+              textColor={textColor}
+            />
           )}
           {headingType === 'image' && headingImage && (
             <div className='flex items-center justify-center'>
@@ -71,17 +79,21 @@ const HeroSectionWrapper = ({
               />
             </div>
           )}
-          <HeroDescription description={description} isLarge={isLarge} />
+          <HeroDescription
+            description={description}
+            isLarge={isLarge}
+            textColor={textColor}
+          />
           <div className='flex justify-center gap-5'>
             <CTAButton
               label={CTAButtonLabel}
-              url={'back-to-school'}
+              url={CTAButtonLink}
               isLarge={isLarge}
             />
             {SecondCTAButtonLabel && (
               <CTAButton
                 label={SecondCTAButtonLabel}
-                url={'back-to-school'}
+                url={CTAButtonLink}
                 isLarge={isLarge}
               />
             )}
