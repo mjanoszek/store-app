a5c8b06ecdea02aa5ac0c634b2c4db305868be9d
mjanoszek <mjanoszek@protonmail.com>
2023-09-23 11:17:14 +0200

    feat: add textColor prop to customize description text color

--- src/app/components/HeroDescription.tsx
+++ src/app/components/HeroDescription.tsx
@@ -3,13 +3,21 @@ import React from 'react';
 interface HeroDescriptionProps {
   description: string | JSX.Element;
   isLarge?: boolean;
+  textColor?: 'black' | 'white';
 }
-const HeroDescription = ({ description, isLarge }: HeroDescriptionProps) => {
+
+const HeroDescription = ({
+  description,
+  isLarge,
+  textColor,
+}: HeroDescriptionProps) => {
   return (
     <p
-      className={
-        isLarge ? 'text-lg md:text-xl lg:text-2xl' : 'text-xl font-light flex justify-center items-center'
-      }
+      className={`${
+        isLarge
+          ? 'text-lg md:text-xl lg:text-2xl'
+          : 'flex items-center justify-center text-xl font-light'
+      } text-${textColor}`}
     >
       {description}
     </p>
